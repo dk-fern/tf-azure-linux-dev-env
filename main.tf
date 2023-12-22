b00311c296c56cf34796534eb56ebbae29226bc4
dkainfernandez <110493897+dkainfernandez@users.noreply.github.com>
2023-12-22 14:36:49 -0800

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,136 @@
+# Config needs: 
+# Resource Group, VM, Public IP, NSG, VNet, subnet, Nic, Disk
+
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Create resource group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "westus3"
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team        = "DevOps"
+  }
+}
+
+# Create vnet
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "West US 3"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create a subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "Subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create an NSG with 'Allow SSH' rule
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = "100"
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# associate nsg with subnet
+resource "azurerm_subnet_network_security_group_association" "nsg_subnet" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Create a public ip -- after deployment show with: "terraform state show <public IP name>" <-- can be easily found with "terraform state list"
+resource "azurerm_public_ip" "public_ip" {
+  name                = "public_ip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_virtual_network.vnet.location
+  allocation_method   = "Dynamic"
+}
+
+# Create a NIC
+resource "azurerm_network_interface" "nic" {
+  name                = "nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+# Create VM
+resource "azurerm_linux_virtual_machine" "linuxvm" {
+  name                  = "linuxvm"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_DS1_v2"
+  admin_username        = "adminuser"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  custom_data = filebase64("customdata.tpl")
+
+  # create keypair: ssh-keygen -t rsa -- saved to C:\Users\fernandezd/.ssh/terraformpracticekey
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/terraformpracticekey.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  provisioner "local-exec" {
+    command = templatefile("${var.host_os}-ssh-script.tpl", {
+      hostname     = self.public_ip_address,
+      user         = "adminuser",
+      identityfile = "~/.ssh/terraformpracticekey"
+    })
+    interpreter = var.host_os == "windows" ? ["Powershell", "-Command"] : ["bash", "-c"]
+  }
+
+}
+
+data "azurerm_public_ip" "publicip_data" {
+  name                = azurerm_public_ip.public_ip.name
+  resource_group_name = azurerm_resource_group.rg.name
+}
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "resource_group_name" {
+  default = "myTFResourceGroup"
+}
+
+variable "host_os" {
+  type    = string
+  default = "windows"
+}
